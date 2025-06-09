fc500a96f67c879292b4c3b19086897629e10ae9
Connor Sullivan <devs@Connors-MacBook-Pro.local>
2025-06-09 09:19:26 -0500

    Add start script for Render (local edit)

---
+++ server/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+console.log('Starting app...');
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Film Permit API is running');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
