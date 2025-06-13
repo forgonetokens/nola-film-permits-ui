59e9277171c87925c463fd2e1168bc9a55a9ab4d
Connor Sullivan <devs@Connors-MacBook-Pro.local>
2025-06-13 07:15:48 -0500

    Redirect root route to /submit to fix blank screen on initial load

--- nola-film-permits-ui/src/App.jsx
+++ nola-film-permits-ui/src/App.jsx
@@ -1,16 +1,16 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import Layout from './components/Layout'
 import Submit from './pages/Submit'
 import View from './pages/View'
 import Admin from './pages/Admin'
-import Home from './pages/Home'
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
+          {/* Redirect root (/#/) to /submit */}
+          <Route index element={<Navigate to="/submit" />} />
           <Route path="submit" element={<Submit />} />
           <Route path="view" element={<View />} />
           <Route path="admin" element={<Admin />} />
